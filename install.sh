93c910eba6c66a51e07b19641639397e2880a6a9
Ali Abbas <ali@alouche.net>
2015-05-17 17:20:34 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@
 local_path="$(dirname "${BASH_SOURCE}")"
 local_path="`( cd \"$local_path\" && pwd )`"
 
-git pull origin master
-git submodule sync
+git pull
 git submodule update --init --recursive
 
 exclude=(install.sh README.mkd shell)
 
 sed -i "/^DOTPATH=/s,=.*,=${local_path}," "zshrc"
+
+ln -sfv "$PWD/shell/prezto" "$HOME/.zprezto"
 
 for file in *; do
   if ! [[ ${exclude[*]} =~ $file ]]; then
@@ -19,11 +20,3 @@ for file in *; do
   fi
 done
 
-pushd . > /dev/null
-cd "$PWD/shell/prezto"
-git submodule update --init --recursive
-popd > /dev/null
-
-ln -sfv "$PWD/shell/prezto" "$HOME/.zprezto"
-
-git submodule foreach git pull origin master
